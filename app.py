1f98d578c5619a5fcd16272473068ef4945ca944
Antoni Kiszka <antoni@kiszka.ovh>
2023-06-30 11:48:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import torch
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-# Ini t is ran on server startup
+# Init is ran on server startup
 # Load your model to GPU as a global variable here.
 def init():
     global model
